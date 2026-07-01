cd993d1cc557a5aa96c951828f210568d2e8eed8
kha.rommy <kha.rommy@gmail.com>
2026-07-01 23:52:24 +0700

    done

--- net-todo-fastendpoints/FirstFastEndpoints/obj/Debug/net10.0/FirstFastEndpoints.AssemblyInfo.cs
+++ net-todo-fastendpoints/FirstFastEndpoints/obj/Debug/net10.0/FirstFastEndpoints.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstFastEndpoints")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b5af11595bc4c46b3ccef20da301dc7a0c75d17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47ccf314a9fd2d7def7ea918c80a41aba470f7ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstFastEndpoints")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstFastEndpoints")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
