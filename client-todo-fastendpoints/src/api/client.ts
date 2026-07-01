123f50738332b52a5b62db020b15e16e76b2defc
kha.rommy <kha.rommy@gmail.com>
2026-07-01 22:47:46 +0700

    update

--- client-todo-fastendpoints/src/api/client.ts
+++ client-todo-fastendpoints/src/api/client.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-const TOKEN_KEY = 'accessToken';
+const TOKEN_KEY = "accessToken";
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5030',
+  // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5030',
 });
 
 client.interceptors.request.use((config) => {
